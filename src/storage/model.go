f09be8773e7873e5a20176be61edbca009981a77
Vinh Truong <56877552+thvinhtruong@users.noreply.github.com>
2023-04-20 22:54:23 +0700

    test

--- src/storage/model.go
+++ src/storage/model.go
@@ -1,1 +1,14 @@
 package storage
+
+type Storage struct {
+	key   string
+	value string
+}
+
+func (s *Storage) Key() string {
+	return s.key
+}
+
+func (s *Storage) Value() string {
+	return s.value
+}
